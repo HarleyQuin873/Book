5cfc31bd1a2755d02357666cc9801dc3dcc2f851
HarleyQuin873 <marina.pugliese3@libero.it>
2026-02-23 17:05:33 +0100

    Fix strict mode errors in HeaderComponent and improve typing

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,21 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit} from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
+import { trigger, state, transition, style, animate} from '@angular/animations';
+import { NgbCollapse } from "@ng-bootstrap/ng-bootstrap";
+
+
 
 @Component({
   selector: 'app-header',
    standalone: true, // obbligatorio
-   imports: [CommonModule, FormsModule, RouterModule],
+   imports: [CommonModule, FormsModule, RouterModule, NgbCollapse],
   template: `
-    <!-- <p>
-      header works!
-    </p> -->
-
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <nav class="animated fadeInDown navbar navbar-expand-lg navbar-light fixed-top" id="mainNav"
+    [@fade]="state">
+      <!-- <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav"> -->
       <div class="container">
-        <a class="navbar-brand" routerLink=""><i class="fa fa-book"></i>Book</a>
+        <a class="navbar-brand" routerLink=""><i class="fa fa-book"></i>Book</a>       
         <button
           class="navbar-toggler navbar-toggler-right"
           type="button"
@@ -24,145 +28,231 @@ import { RouterModule } from '@angular/router';
           aria-controls="navbarResponsive"
           aria-expanded="false"
           aria-label="Toggle navigation"
-        >
+          (click)="isCollapsed = !isCollapsed">
           Menu
           <i class="fas fa-bars"></i>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div [ngbCollapse]="!isCollapsed" class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" routerLink="dashboard">Book</a>
-            </li>
-            <!-- <li class="nav-item">
-              <a class="nav-link" href="about.html">About</a>
-            </li> -->
-            <!-- <li class="nav-item">
-            <a class="nav-link" href="post.html">Sample Post</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
-          </li> -->
           </ul>
         </div>
       </div>
     </nav>
+            <!-- <li class="nav-item">
+              <a class="nav-link" routerLink="dashboard">Book</a>
+            </li>
+           
+          </ul>
+        </div>
+      </div>
+    </nav> -->
 
     <!-- Page Header -->
     <header
-      class="masthead"
-      style="background-image: url('../../../assets/img/home-bg.jpg')"
-    >
+      class="masthead bgImg">
+      <!-- style="background-image: url('../../../assets/img/home-bg.jpg')"
+    > -->
       <div class="overlay"></div>
       <div class="container">
         <div class="row">
           <div class="col-lg-8 col-md-10 mx-auto">
             <div class="site-heading">
-              <h1 class="title">I love book <i class="fa fa-heart"></i></h1>
+              <h1 class="title">I love book <i [style.color]='red' class="fa fa-heart"
+              aria-hidden="true"></i></h1>
               <span class="subheading"
-                >La bellezza non è che una promessa di felicità. <br /><small
-                  ><i>(Stendhal, Dell'amore)</i></small
-                >
+                >La bellezza non è che una promessa di felicità. <br>
+                  <small>
+                    <i>(Stendhal, Dell'amore)</i>
+                  </small>
               </span>
             </div>
           </div>
         </div>
       </div>
     </header>
+    <div class="subHeader"></div>
   `,
 
   styles: [
     `
+      .subHeader{
+        width: 100%;
+        height: 60px;
+        border: 1px solid #dadada;
+        margin-top: 0 !important;
+        margin-bottom: 20px;
+        background: #f3f3f3f3;
+        padding-top: 0px;
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px rgba(0,0,0,0.22);
+      }
+      .subHeader ul li{
+        display: table-cell;
+        vertical-align: middle;
+      }
+      .subHeader ul li a{
+        font-family: 'Montserrat', sans-serif;
+        color: #424242;
+        text-transform: uppercase;
+        font-weight: bold;
+      }
+      .subHeader > ul > li > a {
+        background-color: #f3f3f3f3;
+        border-right: 1px solid #d3d3d3;
+        padding-left: 180px;
+        right: 30px;
+        padding-top: 0;
+        height: 50px !important;
+        display: table-cell;
+        width: 25% !important;
+        vertical-align: middle;
+      }
+      .sticky{
+        background: #fff !important;
+        box-shadow: 0 3px 12px 0 rgba(0,0,0,0.7);
+        position: fixed !important;
+        color: #444 !important;
+        top: 0 !important;
+        height: auto;
+        max-height: 50px;
+        float: left;
+        color: #333 !important;
+      }
+      .sticky a.navbar-brand{
+        color: #222 !important;
+        font-weight: bold !important;
+      }
+      .sticky li a{
+        color: #222 !important;
+        font-weight: bold !important;
+      }
       .bgImg {
         background-image: url(../../../assets/img/home-bg.jpg);
-        max-height: 450px;
+        max-height: 300px;
       }
-      .masthead {
+
+      /* .masthead {
         max-height: 450px;
-      }
+      } */
       .fa.fa-book {
         margin-right: 5px !important;
         color: orange;
       }
-      
+      .fa.fa-lock{
+        margin-left: 5px;
+        margin-top: -3px;
+        font-size: 1rem;
+        color: #212529;
+      }
+      .fa.fa-unlock{
+          margin-left: 5px;
+          margin-top: -3px;
+          font-size: 1rem;
+          color: green;
+      }
       /* .title i.fa-heart {
         color: red;
       } */
       /* .site-heading .title i{
   color:white;
 } */
-      .subheading i.fa-heart {
+      /* .subheading i.fa-heart {
         color: red;
-      }
+      } */
       /* .subheading{
   color:white;
 } */
 
       .navbar-brand {
         font-size: 2.5rem;
-
         font-family: 'Amatic SC', cursive;
       }
-      /* .title {
+       .title {
         font-size: 7.2rem !important;
         font-family: 'Amatic SC', cursive;
-        color: #fff;
-        z-index: 4;
-      } */
+        /* color: #fff;
+        z-index: 4; */
+      } 
       .title {
-  color: #fff !important;       /* testo bianco */
-  font-size: 7.2rem !important;
-  font-family: 'Amatic SC', cursive;
-  text-align: center;
-  text-shadow: 2px 2px 4px rgba(0,0,0,0.5);
-}
-.title i.fa-heart {
-  color: red !important;
-}
+        margin-top: -80px !important;
+        font-size: 6rem !important;
+        font-family: 'Amatic SC', cursive;
+      }
+
       #mainNav .navbar-nav > li.nav-item > a {
         font-size: 10px;
         font-weight: 400;
         letter-spacing: 1px;
         text-transform: uppercase;
       }
+      .header.masthead{
+        margin-bottom: 0 !important;
+      }
+
       header.masthead .overlay {
         position: absolute;
         top: 0;
         left: 0;
         height: 100%;
-        max-height: 450px;
+        max-height: 300px;
         width: 100%;
         background-color: #212529;
-        /* opacity: .5; */
-        /* color: white;  */
-      }
-      /* .title {
-  color: #fff; 
-}
-*/
-
-header.masthead .overlay {
-  background-color: rgba(33, 37, 41, 0.5); /* overlay scuro semitrasparente */
-  z-index: 1;
-}
-
-.site-heading {
-  position: relative;
-  z-index: 2; /* sopra l’overlay */
-}
-      .subheading {
-        color: #fff;
+        opacity: .5; 
       }
     `,
   ],
+  animations:[
+    trigger('fade',
+      [
+        state('void', style({opacity:0.9})),
+        transition(':enter',[animate(300)]),
+        transition(':leave',[animate(500)])
+      ]
+    )
+  ]
 })
-export class HeaderComponent {
-  //  red:string;
+export class HeaderComponent implements OnInit{
+    red:string='';
+    isCollapsed: any;
+    linkMenu: any;
+    state: string = 'void';
 
-  constructor() {}
+    constructor(
+      public auth: AuthService,
+      @Inject(DOCUMENT) private document: Document
+    ) {}
+  // constructor(public auth: AuthService, @Inject(DOCUMENT) document) {
+  //   this.linkMenu=[
+  //     {text:'Book',url:''}
+  //   ];
+  // }
+
+//   @HostListener('window:scroll', ['$event'])
+//  // onWindowScroll(e){
+//     onWindowScroll(e: Event): void {
+//     if(window.pageYOffset > 40){
+//       let element = document.getElementById('mainNav');
+//       element.classList.add('sticky');
+//     }
+//     else{
+//       let element = document.getElementById('mainNav');
+//       element.classList.remove('sticky');
+//     }
+//   }
+
+  @HostListener('window:scroll') 
+  onWindowScroll(): void {
+    const element = this.document.getElementById('mainNav');
+
+    if (!element) return;
+
+    if (window.pageYOffset > 40) {
+      element.classList.add('sticky');
+    } else {
+      element.classList.remove('sticky');
+    }
+  }
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    //  this.red = 'red';
+    this.red = 'red';
   }
 }
